52306f425e84dfb33ac33636d0fb141bb951236e
py-46-Alex <90619667+py-46-Alex@users.noreply.github.com>
2021-11-30 14:38:56 +0300

    uptodateded
    utodated

--- fileD3.py
+++ fileD3.py
@@ -1,7 +1,6 @@
 import os
 from pprint import pprint
 
-
 with open('recept.txt', encoding='utf-8') as fale:
     cook_book = {}
     for dinner in fale:
@@ -15,16 +14,16 @@ with open('recept.txt', encoding='utf-8') as fale:
         fale.readline()
 
 def get_shop_list_by_dishes(dishes, person_count):
-    dish_approve = []
-    set_reapeat = {}
-    rezult = {}
     for aprove in dishes:
+        dish_approve = []
         dish_approve.append(aprove)
     for each in (set(dish_approve)):
+        set_reapeat = {}
         set_reapeat[each] = dishes.count(each)
     for dish1 in dishes:
         dish_approve.append(dish1)
     for dish_for_rezult in set(set_reapeat):
+        rezult = {}
         for status in cook_book.get(dish_for_rezult):
             options_mesh = {}
             options_mesh['measure'] = status.get('measure')
